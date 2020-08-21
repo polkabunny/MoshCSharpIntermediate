1799664e6a93dc162260c722f30a2ed6dfc1ede3
Tríona Barrow <triona.barrow@paddypowerbetfair.com>
2020-08-21 11:43:52 +0100

    section 2 question 1

---
+++ Section2/Stopwatch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Thread.Sleep(500);
+            stopwatch.Stop();
+
+            var duration = stopwatch.GetDuration();
+
+            Console.WriteLine($"Duration: {duration}");
+        }
+    }
+}
---
+++ Section2/Stopwatch/Stopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private TimeSpan _duration;
+        private System.Diagnostics.Stopwatch _stopWatch = new System.Diagnostics.Stopwatch();
+
+        public void Start()
+        {
+            if (!_stopWatch.IsRunning)
+            {
+                _stopWatch.Reset();
+                _stopWatch.Start();
+            }
+            else
+            {
+                throw new InvalidOperationException("StopWatch is already running");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_stopWatch.IsRunning)
+            {
+                _stopWatch.Stop();
+                _duration = _stopWatch.Elapsed;
+            }
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return _duration;
+        }
+    }
+}
