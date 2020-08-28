15c081527f115b46198f2cf6bdf421f9eb0c6a69
Tríona Barrow <triona.barrow@paddypowerbetfair.com>
2020-08-28 11:17:37 +0100

    Post solution

---
+++ Section2/Post/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Post
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostDateTime { get; set; }
+        private int _vote = 0;
+
+        public Post() { }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            PostDateTime = DateTime.Now;
+        }
+
+        public void Upvote()
+        {
+            _vote++;
+        }
+
+        public void Downvote()
+        {
+            _vote--;
+        }
+
+        public int GetVoteValue()
+        {
+            return _vote;
+        }
+    }
+}
---
+++ Section2/Post/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("test", "this is a test post");
+            post.Upvote();
+            post.Upvote();
+            post.Upvote();
+            post.Downvote();
+            post.Downvote();
+            post.Downvote();
+            post.Downvote();
+            post.Downvote();
+            Console.WriteLine($"Current votes: {post.GetVoteValue()}");
+        }
+    }
+}
