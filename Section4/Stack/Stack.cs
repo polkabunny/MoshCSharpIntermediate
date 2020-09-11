332b7625615e1081694f056418eeb08568416e7d
Tríona Barrow <triona.barrow@paddypowerbetfair.com>
2020-09-11 11:38:06 +0100

    Add section 4 exercise

---
+++ Section4/Stack/Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stack.Count > 0)
+            {
+                var returnVal = stack.Last();
+                stack.RemoveAt(stack.Count-1);
+                return returnVal;
+            }
+            throw new InvalidOperationException("Cannot pop on empty stack");
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
